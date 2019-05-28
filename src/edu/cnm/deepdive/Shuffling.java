ac5f4729ac766ad324d818ee1fbb221d4fe6ba85
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-05-28 11:12:23 -0600

    Support for remaing primitive types

--- src/edu/cnm/deepdive/Shuffling.java
+++ src/edu/cnm/deepdive/Shuffling.java
@@ -6,13 +6,13 @@ public class Shuffling {
 
   private static Random defaultRng;
 
-  public static   void shuffle(int[] data) {
+  public static void shuffle(int[] data) {
     Random rng = getRandom();
     shuffle(data, rng);
   }
 
   public static void shuffle(int[] data, Random rng) {
-    for (int i = data.length -1; i > 0; i--) {
+    for (int i = data.length - 1; i > 0; i--) {
       int source = rng.nextInt(i + 1);
       int temp = data[i];
       data[i] = data[source];
@@ -20,11 +20,95 @@ public class Shuffling {
     }
   }
 
-  protected static  Random getRandom() {
+  public static void shuffle(byte[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(byte[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      byte temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(short[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(short[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      short temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(char[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(char[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      char temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(long[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(long[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      long temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(float[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(float[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      float temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(double[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(double[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      double temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  protected static Random getRandom() {
     if (defaultRng == null) {
       defaultRng = new Random();
     }
-    return  defaultRng;
+    return defaultRng;
   }
 
 }
